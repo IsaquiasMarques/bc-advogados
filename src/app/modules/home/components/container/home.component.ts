27ccb012de401c4f3d3e24c178257bdcad4da07d
IsaquiasMarques <patisaquias2000@gmail.com>
2024-01-26 11:11:31 +0100

    scroll-event: fade in on scroll completed

--- src/app/modules/home/components/container/home.component.ts
+++ src/app/modules/home/components/container/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  
+  @HostListener('window:scroll', ['$event']) scrollEvent(){
+
+    let windowHeight = window.innerHeight;
+    let sliderLeft = document.querySelectorAll('div.slide-from-left') as NodeListOf<HTMLElement> || null;
+    let sliderRight = document.querySelectorAll('div.slide-from-right') as NodeListOf<HTMLElement> || null;
+    
+    let sliderBottom = document.querySelectorAll('div.slide-from-bottom') as NodeListOf<HTMLElement> || null;
+    
+    sliderLeft.forEach(element => {
+      let revealTop = element.getBoundingClientRect().top;
+      let revealPoint = 150;
+
+      if(revealTop < (windowHeight - revealPoint)){
+        element.classList.add('active');
+      }else{
+        element.classList.remove('active');
+      }
+
+    });
+
+    sliderRight.forEach(element => {
+      let revealTop = element.getBoundingClientRect().top;
+      let revealPoint = 150;
+
+      if(revealTop < (windowHeight - revealPoint)){
+        element.classList.add('active');
+      }else{
+        element.classList.remove('active');
+      }
+    });
+
+    sliderBottom.forEach(element => {
+      let revealTop = element.getBoundingClientRect().top;
+      let revealPoint = 150 + 0;
+
+      if(revealTop < (windowHeight - revealPoint)){
+        element.classList.add('active');
+      }else{
+        element.classList.remove('active');
+      }
+    });
+
+  }
 
 }
